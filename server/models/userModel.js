fe6e5fb3437ec6b1d532c6f827826e6f22fc8992
Atul Pandey <101567257+AtulPandey429@users.noreply.github.com>
2024-01-18 15:56:32 +0530

    middleware added

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,27 +1,34 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authControllers.js";
+import { requireSignIn } from './../middlewares/authMiddlewares.js';
 const routes = express.Router();
 
 //register route
 routes.post("/register", registerUser);
 
 //login route
-routes.post("/login", loginUser);
+routes.post("/login", requireSignIn, loginUser);
 export default routes;
