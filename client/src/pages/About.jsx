4252270afd8e5e8e532651191f9c653dbf98f313
Atul Pandey <pandeyatul429x@gmail.com>
2024-04-06 13:43:52 +0530

    created it

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-import Layout from "../components/layout/Layout";
-
+import Layout from "../components/Layout/Layout";
+import { Row, Col } from "antd"; // Import Ant Design components for grid layout
 
 const About = () => {
   return (
-    <Layout title={'About-us'}>
-      <div className="row contactus ">
-        <div className="col-md-6 ">
-          <img
-            src="/images/about.jpeg"
-            alt="contactus"
-            style={{ width: "100%" }}
-          />
-        </div>
-        <div className="col-md-4">
-          <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
-          </p>
-        </div>
+    <Layout title={"About Us"}>
+      <div className="container mt-5">
+        <Row gutter={[16, 16]}> {/* Add gutter spacing between columns */}
+          {/* Left column with image */}
+          <Col xs={24} md={10}>
+            <img
+              src="/images/about.jpeg"
+              alt="About Us"
+              style={{ width: "100%", borderRadius: "8px" }} // Add border radius for image
+            />
+          </Col>
+          {/* Right column with text content */}
+          <Col xs={24} md={14}>
+            <div className="p-4" style={{ border: "1px solid #e4e4e4", borderRadius: "8px" }}>
+              <h2 className="mb-3">Our Story</h2>
+              <p className="text-justify">
+                Welcome to our world of creativity and innovation. At [Your Company Name], we strive to deliver the best products and services to our customers. Our journey began with a vision to make a difference in the world, and today, we are proud to have achieved many milestones along the way.
+              </p>
+            </div>
+          </Col>
+        </Row>
       </div>
     </Layout>
   );
